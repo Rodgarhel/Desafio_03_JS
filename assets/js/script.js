f71612ba0b7a8eaf3ed21aba77784b52cb76f949
Rodgarhel <rodgar.heliz@gmail.com>
2026-07-12 21:25:16 -0400

    feat: indexpage and pintar  html.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,5 @@
 const ele = document.getElementById("ele1")
-/*
-function pintar(color = 'green'){
-    
-ele.style.backgroundColor = color
-}
 
-ele.addEventListener("click", pintar('yellow'));
-*/
 function pintar(clicked, color = "green"){
     clicked.style.backgroundColor = color};
 
